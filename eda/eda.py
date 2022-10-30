a3106a8205313e5a52b304491cb97476967b1727
Maneesh Vidhate <maneeshvidhate@ManBase.local>
2022-10-30 00:37:49 -0400

    dataset eda update 1

--- eda/eda.py
+++ eda/eda.py
@@ -22,17 +22,17 @@ def main(df):
     print('\nDATASET ATTRIBUTES: \n', df.dtypes)
     print('\nDATASET TARGET DISTRIBUTION: \n', df.groupby(['isFraud']).count().iloc[:, 0])
 
-    # # class distribution graph
-    # fig = plt.figure(figsize=(16,9))
-    # # graph labels, values, colors
-    # labels = ['Valid', 'Fraud']
-    # sizes = df.groupby(['isFraud']).count().iloc[:, 0].to_numpy()
-    # colors = sns.color_palette('pastel')[0:2]
-    # explode = (0.2, 0.0)
-    # # plot graph
-    # plt.pie(sizes, labels=labels,
-    #         explode=explode, colors=colors, autopct='%1.2f%%', startangle=60)
-    # plt.axis('equal')
-    # plt.show()
+    # class distribution graph
+    fig = plt.figure(figsize=(16,9))
+    # graph labels, values, colors
+    labels = ['Valid', 'Fraud']
+    sizes = df.groupby(['isFraud']).count().iloc[:, 0].to_numpy()
+    colors = sns.color_palette('pastel')[0:2]
+    explode = (0.2, 0.0)
+    # plot graph
+    plt.pie(sizes, labels=labels,
+            explode=explode, colors=colors, autopct='%1.2f%%', startangle=60)
+    plt.axis('equal')
+    plt.show()
 
     return
